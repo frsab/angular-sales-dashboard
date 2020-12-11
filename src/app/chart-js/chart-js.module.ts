1f09fd4f84abe365edc631adf5d933257bd0f6c4
Saber FRAJ <frejsaber@gmail.com>
2020-12-11 20:56:17 +0100

    import ng2-charts in  empty module for ng2-chart samples

--- src/app/chart-js/chart-js.module.ts
+++ src/app/chart-js/chart-js.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ChartJsRoutingModule } from './chart-js-routing.module';
 import { ChartJsComponent } from './chart-js.component';
+import {ChartsModule} from 'ng2-charts';
 
 
 @NgModule({
   declarations: [ChartJsComponent],
   imports: [
     CommonModule,
-    ChartJsRoutingModule
+    ChartJsRoutingModule,
+    ChartsModule
   ]
 })
 export class ChartJsModule { }
